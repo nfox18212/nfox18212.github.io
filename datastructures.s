2a13a2c7acb983569bb608bbe032a238eee2250c
nfox18212 <nfox18212@gmail.com>
2024-04-02 09:43:51 -0400

    worked on data structures

--- datastructures.s
+++ datastructures.s
@@ -1,4 +1,17 @@
 
-.data
+	.data
 
-mats: 	.space 	128
+mats: 		.space 	128 	; matrix storage, size will need to change
+adj_list:	.space 	512		; adjacency list, size will need to change
+
+	.text
+	.global init_datastructures
+
+
+matrix_list:	.word	mats
+alist:	.word	adj_list
+
+init_datastructures:
+
+	push	{r4-r12,lr}
+	ldr		r4, alist		; first
