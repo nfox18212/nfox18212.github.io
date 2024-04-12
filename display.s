5473ec7ee1c31c0843001de5f7be430154823053
sjbowen01 <sjbowen@buffalo.edu>
2024-04-12 12:46:35 -0400

    ermmm just a few lines

--- display.s
+++ display.s
@@ -1,2 +1,30 @@
-; this is sebastien's file
+; COLOR STRINGS
+; background (squares)
+redbg:      .string 27,"[48;5;31H"
+greenbg:    .string 27,"[48;5;32H"
+yellowbg:   .string 27,"[48;5;33H"
+bluebg:     .string 27,"[48;5;34H"
+magentabg:  .string 27,"[48;5;35H"
+cyanbg:     .string 27,"[48;5;36H"
 
+; foreground (player)
+redfg:      .string 27,"[38;5;31H"
+greenfg:    .string 27,"[38;5;32H"
+yellowfg:   .string 27,"[38;5;33H"
+bluefg:     .string 27,"[38;5;34H"
+magentafg:  .string 27,"[38;5;35H"
+cyanfg:     .string 27,"[38;5;36H"
+
+; BUILD DISPLAY MATRIX
+
+
+
+; column reflection subroutine
+
+; transpose subroutine
+
+; APPLY PROPER ROTATION AND OUTPUT
+
+
+
+; animate rotation of cube... right, left, up, down
